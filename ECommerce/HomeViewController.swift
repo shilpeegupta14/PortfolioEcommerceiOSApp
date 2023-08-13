da76a8ac972733739d86ca6fa70675ac83e1b5a6
Aditya Vyavahare <vvaditya23@icloud.com>
2023-08-13 23:25:37 +0530

    Manually added Aditya's code to main branch.

---
+++ ECommerce/HomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeViewController.swift
+//  ECommerce
+//
+//  Created by aditya sharma on 13/08/23.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+
+    @IBOutlet weak var tableView: UITableView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension HomeViewController: UITableViewDelegate {
+
+}
+
+extension HomeViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CEnumClass.sharedEnum.cateArr.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
+        cell.titleLabel.text = CEnumClass.sharedEnum.cateArr[indexPath.row]
+        cell.totalProductLabel.text = CEnumClass.sharedEnum.totalItemArr[indexPath.row]
+        cell.iconImageVIew.image = UIImage(systemName: CEnumClass.sharedEnum.cateIconArr[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+
+}
